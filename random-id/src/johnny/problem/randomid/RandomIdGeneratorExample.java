a3771cf5bf405212ab9314141c1de795dda5f6f5
Zhuang, Rong <jojozhuang@gmail.com>
2019-08-01 21:36:16 -0700

    refine random id

--- random-id/src/johnny/problem/randomid/RandomIdGeneratorExample.java
+++ random-id/src/johnny/problem/randomid/RandomIdGeneratorExample.java
@@ -2,7 +2,6 @@ package johnny.problem.randomid;
 
 public class RandomIdGeneratorExample {
     public static void main(String[] args) {
-
         System.out.println("Create 5 IDs with length of 6, base62");
         // Create 5 IDs with length of 6, base62
         for (int i = 0; i < 5; i++) {
